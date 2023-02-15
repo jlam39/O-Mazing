06b7a6d02ca8131eb673b7f0cf99d68e5e5c76e9
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-02-15 10:34:58 -0800

    Speed of the player class added and can be initialized

--- src/main/java/classes/Player.java
+++ src/main/java/classes/Player.java
@@ -11,12 +11,14 @@ public class Player extends JPanel implements KeyListener{
 	private int x;
 	private int y;
 	private Position p; //added position class, might want to use this
+	private int speed; // added speed to Player class so it is easy to change
 	private int ballSize;
 	
 
-public Player(int x, int y, int ballSize) {
+public Player(int x, int y, int s, int ballSize) {
 	this.x = x;
 	this.y = y;
+	this.speed = s;
 	this.p = new Position(x, y); // initialized position
 	this.ballSize = ballSize;
 	this.setFocusable(true);
@@ -29,24 +31,26 @@ public void paintComponent(Graphics g) {
 	g.fillOval(x,  y, ballSize, ballSize);
 }
 
+//movement with a set speed of "speed"
+
 public void moveLeft() {
-	x -= 5;
+	x -= speed;
 	this.p = new Position(x, y);
 	
 }
 
 public void moveRight() {
-	x += 5;
+	x += speed;
 	this.p = new Position(x, y);
 }
 
 public void moveUp () {
-	y -= 5;
+	y -= speed;
 	this.p = new Position(x, y);
 }
 
 public void moveDown () {
-	y += 5;
+	y += speed;
 	this.p = new Position(x, y);
 }
 
--- src/main/java/classes/main.java
+++ src/main/java/classes/main.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class main {
     public static void main(String[] args) {
-        Player player = new Player(100, 100, 50);
+        Player player = new Player(100, 100, 5, 50);
         JFrame frame = new JFrame();
         frame.add(player);
         frame.setSize(500, 500);
