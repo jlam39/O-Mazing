b122eaccac8df2f29d17b212263149253e21447e
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-02-14 23:18:34 -0800

    Added buff Type condition and added None type to Buff_Type java

--- src/main/java/classes/Buff_Type.java
+++ src/main/java/classes/Buff_Type.java
@@ -2,7 +2,7 @@ package classes;
 
 public enum Buff_Type {
 
-	O1, logN, N2;
+	O1, logN, N2, None;
 
 	public String toString() {
 		switch (this) {
@@ -12,7 +12,10 @@ public enum Buff_Type {
 			return "logN";
 		case N2:
 			return "N2";
+		case None:
+			return "None";
 		}
+
 			
 		return "n/a";
 	}
