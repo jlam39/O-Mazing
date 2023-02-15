9a2e58ce552c4874a1d0e80e43191fe69c633d16
johnlam <johnlam@10.0.0.125>
2023-02-14 23:31:00 -0800

    Added movement options such as arrow keys and wasd

--- src/main/java/classes/Player.java
+++ src/main/java/classes/Player.java
@@ -39,4 +39,17 @@ public void moveDown () {
 	y += 5;
 }
 
+public void keyPressed(KeyEvent e) {
+    int keyCode = e.getKeyCode();
+    if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
+        moveLeft();
+    } else if (keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D) {
+        moveRight();
+    } else if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_W) {
+        moveUp();
+    } else if (keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_S) {
+        moveDown();
+    }
+    
+}
 }
