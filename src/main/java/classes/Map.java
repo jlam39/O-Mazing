ffbc58070e193c655fb7c291bd4f76a612e76981
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-02-14 22:57:43 -0800

    Added Map Coordinate class which represents each location in Map grid

---
+++ src/main/java/classes/Map.java
@@ -0,0 +1,34 @@
+package classes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.Timer;
+
+import acm.program.GraphicsProgram;
+import edu.pacific.comp55.starter.GraphicsApplication;
+import edu.pacific.comp55.starter.MainApplication;
+
+public class Map extends GraphicsApplication implements ActionListener, KeyListener {
+	// Map will be a grid with grid blocks
+	
+	
+	
+	
+	
+
+	@Override
+	public void run() {
+		
+		
+	}
+	
+	public static void main(String args[]) {
+		new Map().start();
+	}
+
+
+}
