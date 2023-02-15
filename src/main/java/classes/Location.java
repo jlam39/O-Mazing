b149be1d53446fddf634d363e54370d41c887bc3
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-02-14 23:36:59 -0800

    Initialized Location

---
+++ src/main/java/classes/Location.java
@@ -0,0 +1,18 @@
+package classes;
+
+import acm.graphics.*;
+
+public class Location {
+	private Terrain t;
+	private Position p; 
+	private Buff_Type b;
+	Location(Terrain terr, Position pos, Buff_Type buff){
+		t = terr;
+		p = pos;
+		b = buff;
+		
+	}
+ 
+
+
+}
