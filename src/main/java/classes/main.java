42173ef0ba8c8c9f08c5da282578dec9dff32b97
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-02-15 23:28:53 -0800

    Created Map Array full of Location

--- src/main/java/classes/main.java
+++ src/main/java/classes/main.java
@@ -5,9 +5,11 @@ public class main {
     public static void main(String[] args) {
         Player player = new Player(100, 100, 5, 50);
         JFrame frame = new JFrame();
-        frame.add(player);
+        
         frame.setSize(500, 500);
         frame.pack();
+        frame.add(player);
         frame.setVisible(true);
+        
     }
 }
