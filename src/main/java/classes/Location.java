a59f9879868d5b8ebabdd02a71d66909c006124b
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-02-15 23:44:31 -0800

    Added Block width and height

--- src/main/java/classes/Location.java
+++ src/main/java/classes/Location.java
@@ -6,13 +6,15 @@ public class Location {
 	private Terrain t;
 	private Position p; 
 	private Buff_Debuff b;
-	
+	private int width = 10;
+	private int height = 10;
+	GRect block;
 	// initialize
 	Location(Terrain terr, Position pos, Buff_Debuff buff){
 		this.t = terr;
 		this.p = pos;
 		this.b = buff;
-		
+		block = new GRect(this.p.getX() - width/2, this.p.getY() + height/2, width, height);
 	}
 	
 	//setters
@@ -39,6 +41,8 @@ public class Location {
 	Buff_Debuff get_buff() {
 		return b;
 	}
+	
+	//Create an Image 
 	
  
 
